c3668ed6d56b71bf40e96a8721adaa7f3ae4423e
DennisY888 <dennisyang151@gmail.com>
2024-05-19 10:42:12 -0700

    dennis final version

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from functools import wraps
 import os
 
 import sqlite3
+# import psycopg2
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
@@ -14,6 +15,12 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
+
+
+
+
+
+# DATABASE_URL = "postgresql://postgres:Q7q8q94-20050501@localhost/public.users"
 
 
 
@@ -53,9 +60,16 @@ def login():
             return render_template("apology.html")
 
 
+        
         with sqlite3.connect("users.db") as conn:
             cursor = conn.cursor()
             rows = cursor.execute("SELECT * FROM users WHERE username = ?", (request.form.get("username"),)).fetchall()
+
+        """
+        with psycopg2.connect(DATABASE_URL) as conn:
+            cursor = conn.cursor()
+            rows = cursor.execute("SELECT * FROM users WHERE username = ?", (request.form.get("username"),)).fetchall()
+        """
 
 
         if len(rows) != 1 or not check_password_hash(rows[0][4], request.form.get("password")):
@@ -82,19 +96,32 @@ def register():
             return render_template("apology.html")
 
 
+        
         with sqlite3.connect("users.db") as conn:
             cursor = conn.cursor()
             rows = cursor.execute("SELECT * FROM users WHERE username = ?", (request.form.get("register_username"),)).fetchall()
+        """
+        with psycopg2.connect(DATABASE_URL) as conn:
+            cursor = conn.cursor()
+            rows = cursor.execute("SELECT * FROM users WHERE username = ?", (request.form.get("register_username"),)).fetchall()
+        """
 
 
         if len(rows) != 0:
             return render_template("failed_register.html")
         
 
+        
         with sqlite3.connect("users.db") as conn:
             cursor = conn.cursor()
             rows = cursor.execute("INSERT INTO users (username, reps, score, password) VALUES (?, ?, ?, ?)", (request.form.get("register_username"), 0, 0, generate_password_hash(request.form.get("register_password"))))
             conn.commit()
+        """
+        with psycopg2.connect(DATABASE_URL) as conn:
+            cursor = conn.cursor()
+            rows = cursor.execute("INSERT INTO users (username, reps, score, password) VALUES (?, ?, ?, ?)", (request.form.get("register_username"), 0, 0, generate_password_hash(request.form.get("register_password"))))
+            conn.commit()
+        """
 
 
         return redirect("/login")
@@ -130,18 +157,36 @@ def leaderboard():
         except ValueError:
             return redirect("/leaderboard")
 
+        
         with sqlite3.connect("users.db") as conn:
             cursor = conn.cursor()
             cursor.execute("UPDATE users SET reps = ?, score = ? WHERE username = ?;", (reps, score, session["name"]))
             conn.commit()
+        """
+
+        with psycopg2.connect(DATABASE_URL) as conn:
+            cursor = conn.cursor()
+            cursor.execute("UPDATE users SET reps = ?, score = ? WHERE username = ?;", (reps, score, session["name"]))
+            conn.commit()
+        """
+        
+        
 
         return redirect("/leaderboard")
 
     else:
 
+        
         with sqlite3.connect("users.db") as conn:
             cursor = conn.cursor()
             rows = cursor.execute("SELECT * FROM users ORDER BY reps DESC").fetchall()
+        """
+        
+        with psycopg2.connect(DATABASE_URL) as conn:
+            cursor = conn.cursor()
+            rows = cursor.execute("SELECT * FROM users ORDER BY reps DESC").fetchall()
+        """
+
         return render_template("leaderboard.html", users=rows)
 
 
@@ -152,10 +197,19 @@ def leaderboard():
 def remove():
     id = request.form.get("id")
     if id:
+        
         with sqlite3.connect("users.db") as conn:
             cursor = conn.cursor()
             cursor.execute("DELETE FROM users WHERE id = ?", (id))
             conn.commit()
+        """
+        
+        with psycopg2.connect(DATABASE_URL) as conn:
+            cursor = conn.cursor()
+            cursor.execute("DELETE FROM users WHERE id = ?", (id))
+            conn.commit()
+        """
+
     return redirect("/leaderboard")
 
 
